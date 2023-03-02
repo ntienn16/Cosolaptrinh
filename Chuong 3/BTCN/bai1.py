d5dde7ee400a5d4fbe99922db727ec05965bcfa1
ntienn16 <ngnhuantien@gmail.com>
2023-03-02 07:31:07 +0700

    Ádd new file

--- Chuong 3/BTCN/bai1.py
+++ Chuong 3/BTCN/bai1.py
@@ -4,7 +4,7 @@ b=int(input("b="))
 c=int(input("c="))
 p=(a+b+c)/2
 dt=math.sqrt(p*(p-a)*(p-b)*(p-c))
-if  (a+b)>c and (a+c)>c and (b+c)>a:
+if  (a+b)>c and (a+c)>b and (b+c)>a:
     print("Dien tich=",round(dt,3),sep="")
 else:
     print("Khong hop le")
