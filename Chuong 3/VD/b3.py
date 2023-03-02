48a3eaea5cc9af3d9021c7ebf8b458ef76a96834
ntienn16 <ngnhuantien@gmail.com>
2023-03-02 07:42:05 +0700

    Add new file

--- Chuong 3/VD/b3.py
+++ Chuong 3/VD/b3.py
@@ -1,10 +1,14 @@
 tt=int(input("Tieu thu="))
+g1=550
+g2=750
+g3=950
+g4=1350
 if  tt<=100:
-    tien=tt*550
+    tien=tt*g1
 elif    tt<=150:
-    tien=(100*550)+(tt-100)*750
+    tien=(100*g1)+(tt-100)*g2
 elif    tt<=250:
-    tien=(100*550)+(50*750)+(tt-150)*950
-elif    tien<=201:
-    tien=(100*550)+(50*750)+(50*950)+(tt-200)*1350
+    tien=(100*g1)+(50*g2)+(tt-150)*g3
+else:
+    tien=(100*g1)+(50*g2)+(50*g3)+(tt-200)*g4
 print("Phai tra=",tien+(tien*10)/100,sep="")
