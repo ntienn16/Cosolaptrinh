2f53f7a45b9f49237d35039df8dfb6ece3b82bf5
ntienn16 <ngnhuantien@gmail.com>
2023-03-04 14:32:39 +0700

    add

---
+++ Chuong 3/Bai32.py
@@ -0,0 +1,7 @@
+n=int(input("Nhap n="))
+i=1
+while   i<=n:
+        print(i,end="")
+        i=i+1
+print("\n")
+    
---
+++ Chuong 3/bai31.py
@@ -0,0 +1,6 @@
+while True:
+    n=int(input("Nhap so n="))
+    if  n<1 or n>50:
+        print("Moi nhap lai")
+    else:
+        break
