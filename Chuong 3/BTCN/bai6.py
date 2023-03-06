20946f232371459ce9cdcb9dd7c7a8e97361410b
ntienn16 <ngnhuantien@gmail.com>
2023-03-06 21:52:26 +0700

    add

--- Chuong 3/BTCN/bai6.py
+++ Chuong 3/BTCN/bai6.py
@@ -1,7 +1,7 @@
 import math
-a=int(input("a="))
-b=int(input("b="))
-c=int(input("c="))
+a=float(input(""))
+b=float(input(""))
+c=float(input(""))
 if  (a+b)>c and (a+c)>b and (b+c)>a and a>0 and b>0 and c>0:
     if  a*a==b*b+c*c or b*b==a*a+c*c or c*c==a*a+b*b:
         print("Tam giac vuong")
